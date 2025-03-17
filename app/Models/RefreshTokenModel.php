3221de83084f1d9d3642641a71a5028854d874eb
telo <erfin.is.dev@gmail.com>
2025-03-17 15:35:35 +0700

    updated_at

--- app/Models/RefreshTokenModel.php
+++ app/Models/RefreshTokenModel.php
@@ -12,7 +12,7 @@ class RefreshTokenModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    =  ['user_id', 'token', 'expires_at'];
+    protected $allowedFields    =  ['user_id', 'token', 'expires_at','updated_at','created_at'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
